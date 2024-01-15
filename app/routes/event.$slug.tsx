ce86a9b1035aa1f0d625c5fb588d7d3a81b01833
Nicholas <nicholas.griffin@bbc.co.uk>
2024-01-15 23:10:31 +0000

    chore: adding mobile variant for event page

--- app/routes/event.$slug.tsx
+++ app/routes/event.$slug.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction, LoaderFunctionArgs } from '@remix-run/cloudflare';
 import { json } from '@remix-run/cloudflare';
 import { useLoaderData, useRouteLoaderData } from '@remix-run/react';
+import clsx from 'clsx';
 
 import {
   ResizableHandle,
@@ -12,6 +13,7 @@ import { PageHeader } from '~/components/PageHeader';
 import { Icons } from '~/components/Icons';
 import { Button } from '~/components/ui/button';
 import { toast } from '~/components/ui/use-toast';
+import { useWindowSize } from '~/hooks/useWindowSize';
 
 export const meta: MetaFunction = () => {
   return [
@@ -24,9 +26,86 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
   return json({ slug: params.slug });
 };
 
+function EventPlayer({ event, isMobile = false }) {
+  const videoWrapperClassName = clsx(
+    'flex h-full items-center justify-center p-6 bg-muted text-muted-foreground',
+    {
+      'min-h-[340px] rounded-md': isMobile,
+      'min-h-[420px]': !isMobile,
+    }
+  );
+  return (
+    <div className={videoWrapperClassName}>
+      {event.isLive && event.streams?.length ? (
+        <div>
+          {event.streams.map((stream) => {
+            return (
+              <div key={stream.url} className="w-full aspect-video">
+                <video preload="none" controls="controls">
+                  <source
+                    src={stream.url}
+                    type='application/x-mpegURL; codecs="avc1.42E01E, mp4a.40.2"'
+                  />
+                </video>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div>
+          <span>Sorry! The stream isn't available yet!</span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function EventSidebar({ event, isMobile = false }) {
+  const sidebarClassName = clsx('h-full', {
+    'p-6': !isMobile,
+    'pt-6 pb-6': isMobile,
+  });
+
+  const abstractClassName = clsx(
+    'w-full prose prose-lg prose-indigo overflow-scroll',
+    {
+      'max-h-[420px]': !isMobile,
+    }
+  );
+
+  return (
+    <div className={sidebarClassName}>
+      {event.abstract && (
+        <div className={abstractClassName}>
+          <div dangerouslySetInnerHTML={{ __html: event.abstract }} />
+        </div>
+      )}
+      {event.links?.length > 0 && (
+        <div>
+          <hr className="my-4" />
+          <h2>Links</h2>
+          <ul>
+            {event.links.map((link) => {
+              return (
+                <li key={link.href}>
+                  <a href={link.href} target="_blank" rel="noreferrer">
+                    {link.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
 export default function TrackPage() {
   const { slug } = useLoaderData<typeof loader>();
   const { fosdem } = useRouteLoaderData('root');
+
+  const { width } = useWindowSize();
 
   if (!fosdem) return null;
 
@@ -67,66 +146,25 @@ export default function TrackPage() {
           </div>
         </PageHeader>
         <div className="w-full">
-          <ResizablePanelGroup
-            direction="horizontal"
-            className="min-h-[200px] rounded-lg border"
-          >
-            <ResizablePanel defaultSize={75}>
-              <div className="flex h-full items-center justify-center p-6 bg-muted text-muted-foreground">
-                {event.isLive && event.streams?.length ? (
-                  <div>
-                    {event.streams.map((stream) => {
-                      return (
-                        <div key={stream.url} className="w-full aspect-video">
-                          <video preload="none" controls="controls">
-                            <source
-                              src={stream.url}
-                              type='application/x-mpegURL; codecs="avc1.42E01E, mp4a.40.2"'
-                            />
-                          </video>
-                        </div>
-                      );
-                    })}
-                  </div>
-                ) : (
-                  <div>
-                    <span>Sorry! The stream isn't available yet!</span>
-                  </div>
-                )}
-              </div>
-            </ResizablePanel>
-            <ResizableHandle withHandle />
-            <ResizablePanel defaultSize={25}>
-              <div className="h-full p-6">
-                {event.abstract && (
-                  <div className="max-h-[420px] w-full prose prose-lg prose-indigo overflow-scroll">
-                    <div dangerouslySetInnerHTML={{ __html: event.abstract }} />
-                  </div>
-                )}
-                {event.links?.length > 0 && (
-                  <div>
-                    <hr className="my-4" />
-                    <h2>Links</h2>
-                    <ul>
-                      {event.links.map((link) => {
-                        return (
-                          <li key={link.href}>
-                            <a
-                              href={link.href}
-                              target="_blank"
-                              rel="noreferrer"
-                            >
-                              {link.title}
-                            </a>
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </div>
-                )}
-              </div>
-            </ResizablePanel>
-          </ResizablePanelGroup>
+          {width < 768 ? (
+            <>
+              <EventPlayer event={event} isMobile />
+              <EventSidebar event={event} isMobile />
+            </>
+          ) : (
+            <ResizablePanelGroup
+              direction="horizontal"
+              className="min-h-[200px] rounded-lg border"
+            >
+              <ResizablePanel defaultSize={75}>
+                <EventPlayer event={event} />
+              </ResizablePanel>
+              <ResizableHandle withHandle />
+              <ResizablePanel defaultSize={25}>
+                <EventSidebar event={event} />
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          )}
           {event.chat && (
             <Alert className="mt-4">
               <Icons.logo className="h-4 w-4" />
