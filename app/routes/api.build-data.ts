0d98ae5385182fc9c36ae5978b3d3679056b6664
Nicholas <nicholas.griffin@bbc.co.uk>
2024-01-15 20:25:14 +0000

    chore: adding a bit more security to build

--- app/routes/api.build-data.ts
+++ app/routes/api.build-data.ts
@@ -4,8 +4,29 @@ import { json } from '@remix-run/cloudflare';
 import type { Env } from '~/types/env';
 import { buildData } from '~/lib/fosdem';
 
-export async function loader({ context }: LoaderFunctionArgs) {
+export async function loader({ request, context }: LoaderFunctionArgs) {
+  const now = new Date();
+  if (
+    now.getFullYear() !== 2024 ||
+    now.getMonth() !== 1 ||
+    now.getDate() < 3 ||
+    now.getDate() > 4
+  ) {
+    return json({
+      status: 403,
+      message: 'Forbidden',
+    });
+  }
+
   const env = context.env as Env;
+
+  const secret = request.headers.get('X-Fosdem-Secret');
+  if (secret !== env.FOSDEM_SECRET) {
+    return json({
+      status: 403,
+      message: 'Forbidden',
+    });
+  }
 
   const fosdem = await buildData({ year: '2024' });
 
