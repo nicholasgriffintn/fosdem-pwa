c0746d1f1708600e249ac68590b26e44c5aa3c2d
Nicholas <nicholas.griffin@bbc.co.uk>
2024-01-16 17:58:32 +0000

    feat: adding better username generation

--- app/services/auth/auth.server.ts
+++ app/services/auth/auth.server.ts
@@ -3,18 +3,7 @@ import type { AppLoadContext } from '@remix-run/cloudflare';
 import { eq } from 'drizzle-orm';
 
 import { getDbFromContext, users } from '~/services/database';
-
-const randomUsername = () => {
-  const a = ['Small', 'Blue', 'Ugly'];
-  const b = ['Bear', 'Dog', 'Banana'];
-
-  const rA = Math.floor(Math.random() * a.length);
-  const rB = Math.floor(Math.random() * b.length);
-
-  const name = a[rA] + b[rB];
-
-  return name;
-};
+import { randomUsername } from '~/lib/username';
 
 export const getUserFromSession = async (
   session: CustomSession,
