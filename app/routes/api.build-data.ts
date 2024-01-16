932850171cca073de6d96a75e5d0e8442f9ef67b
Nicholas <nicholas.griffin@bbc.co.uk>
2024-01-16 18:09:32 +0000

    chore: removing data validation

--- app/routes/api.build-data.ts
+++ app/routes/api.build-data.ts
@@ -5,19 +5,6 @@ import type { Env } from '~/types/env';
 import { buildData } from '~/lib/fosdem';
 
 export async function loader({ request, context }: LoaderFunctionArgs) {
-  const now = new Date();
-  if (
-    now.getFullYear() !== 2024 ||
-    now.getMonth() !== 1 ||
-    now.getDate() < 3 ||
-    now.getDate() > 4
-  ) {
-    return json({
-      status: 403,
-      message: 'Forbidden',
-    });
-  }
-
   const env = context.env as Env;
 
   const secret = request.headers.get('X-Fosdem-Secret');
