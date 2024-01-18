04354a02fd866f70286cb1e0a3d595a9e0804b08
Nicholas <nicholas.griffin@bbc.co.uk>
2024-01-18 02:58:43 +0000

    chore: MOAR IMAGE OPTIMISATION!!!!

--- app/components/Image/Image.tsx
+++ app/components/Image/Image.tsx
@@ -15,9 +15,9 @@ export function Image({
     <div className={className}>
       <picture>
         {useWebP && (
-          <source srcSet={src.replace('.png', '.webp')} type="image/webp" />
+          <source srcSet={src.replace('.jpg', '.webp')} type="image/webp" />
         )}
-        <source srcSet={src} type="image/png" />
+        <source srcSet={src} type="image/jpeg" />
         <img src={src} alt={alt} {...props} />
       </picture>
     </div>
