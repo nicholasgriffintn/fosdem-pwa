b00d6c7827ad14e7982dfa96b68ad595bd1ef021
Nicholas <nicholas.griffin@bbc.co.uk>
2024-12-14 22:26:07 +0000

    feat: clearing out for 2025

--- fixtures/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO 'users' ('id', 'name', 'email', 'type', 'created_at', 'updated_at') VALUES
-(17381137, 'Test Full Account', 'test@nicholasgriffin.co.uk', 'full', '2014-01-01 00:00:00', '2014-01-01 00:00:00'),
-(17381138, 'Test Guest Account', '', 'guest', '2014-01-01 00:00:00', '2014-01-01 00:00:00');
