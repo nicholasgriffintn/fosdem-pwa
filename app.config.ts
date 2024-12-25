cab1727a90f9c99e96a2f307496062d7a6226e2a
Nicholas Griffin <nick@undefined.computer>
2024-12-25 01:39:10 +0000

    chore: adding missing config

--- app.config.ts
+++ app.config.ts
@@ -11,93 +11,10 @@ export default defineConfig({
         projects: ["./tsconfig.json"],
       }),
       VitePWA({
+        injectRegister: null,
         registerType: 'autoUpdate',
-        includeAssets: ['favicon.ico', 'icons/*'],
-        manifest: {
-          name: 'FOSDEM PWA',
-          short_name: 'FOSDEM',
-          description: 'A companion app for FOSDEM conference',
-          theme_color: '#000000',
-          background_color: '#d3d7dd',
-          start_url: '/',
-          display: 'standalone',
-          orientation: 'portrait',
-          shortcuts: [
-            {
-              name: 'Homepage',
-              url: '/',
-              icons: [
-                {
-                  src: '/icons/android-chrome-96x96.png',
-                  sizes: '96x96',
-                  type: 'image/png',
-                  purpose: 'any monochrome',
-                },
-              ],
-            },
-          ],
-          icons: [
-            {
-              src: '/icons/android-chrome-36x36.png',
-              sizes: '36x36',
-              type: 'image/png',
-            },
-            {
-              src: '/icons/android-chrome-48x48.png',
-              sizes: '48x48',
-              type: 'image/png',
-            },
-            {
-              src: '/icons/android-chrome-72x72.png',
-              sizes: '72x72',
-              type: 'image/png',
-            },
-            {
-              src: '/icons/android-chrome-96x96.png',
-              sizes: '96x96',
-              type: 'image/png',
-            },
-            {
-              src: '/icons/android-chrome-144x144.png',
-              sizes: '144x144',
-              type: 'image/png',
-            },
-            {
-              src: '/icons/android-chrome-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: '/icons/android-chrome-256x256.png',
-              sizes: '256x256',
-              type: 'image/png',
-            },
-            {
-              src: '/icons/android-chrome-384x384.png',
-              sizes: '384x384',
-              type: 'image/png',
-            },
-            {
-              src: '/icons/android-chrome-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-          ],
-          screenshots: [
-            {
-              src: 'screenshots/1.jpg',
-              sizes: '1080x1920',
-              type: 'image/webp',
-              form_factor: 'narrow',
-            },
-            {
-              src: 'screenshots/2.png',
-              sizes: '3314x1920',
-              type: 'image/webp',
-              form_factor: 'wide',
-            },
-          ],
-        }
+        includeAssets: ['favicon.ico', 'icons/*', 'images/*'],
+        manifest: false,
       })
     ],
     build: {
