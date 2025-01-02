bf08009202e94c87b86b8257ff619a1811fe1df4
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-01-02 17:53:41 +0000

    chore: switching to biome

--- app/lib/fosdem.ts
+++ app/lib/fosdem.ts
@@ -1,10 +1,11 @@
-import { DayGroupedData } from "~/types/fosdem";
+import type { DayGroupedData } from "~/types/fosdem";
 
 export const groupByDay = (items: any[], getDayFn: (item: any) => string[]): DayGroupedData => {
     return items.reduce((acc: DayGroupedData, item) => {
         const dayValue = getDayFn(item);
         const days = Array.isArray(dayValue) ? dayValue : [String(dayValue)];
 
+        // biome-ignore lint/complexity/noForEach: <explanation>
         days.forEach(day => {
             if (!acc[day]) {
                 acc[day] = [];
