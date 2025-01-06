1245c49eaf577f6895318ad119af14b244f363b6
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-01-06 20:36:38 +0000

    chore: moving env vars from client

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -12,17 +12,14 @@ import { QueryClientProvider } from "@tanstack/react-query";
 
 import appCss from "~/styles/app.css?url";
 import { cn } from "~/lib/utils";
-import { getCloudflareEnv } from "~/server/config";
 import { Header } from "~/components/Header";
 import { Footer } from "~/components/Footer";
 import { Toaster } from "~/components/ui/toaster";
 import { OfflineIndicator } from "~/components/OfflineIndicator";
 import { ServiceWorkerUpdater } from "~/components/ServiceWorkerUpdater";
 
-const env = getCloudflareEnv();
-
 const TanStackRouterDevtools =
-	env.NODE_ENV === "production"
+	process.env.NODE_ENV !== "development"
 		? () => null
 		: lazy(() =>
 			import("@tanstack/router-devtools").then((res) => ({
