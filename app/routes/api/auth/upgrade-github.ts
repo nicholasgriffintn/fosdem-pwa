79309b05ef576f7b154ec177840824890103b94d
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-01-16 22:25:26 +0000

    fix: upgrade with github

--- app/routes/api/auth/upgrade-github.ts
+++ app/routes/api/auth/upgrade-github.ts
@@ -1,26 +1,30 @@
 import { createAPIFileRoute } from "@tanstack/start/api";
+import { generateState } from "arctic";
+import { setCookie, setHeader } from "vinxi/http";
+
 import { github } from "~/server/auth";
-import { setCookie } from "vinxi/http";
+import { getCloudflareEnv } from "~/server/config";
 
 export const APIRoute = createAPIFileRoute("/api/auth/upgrade-github")({
   GET: async () => {
     try {
-      const state = crypto.randomUUID();
-      const url = await github.createAuthorizationURL(state, ["user:email"]);
+      const env = getCloudflareEnv();
+      const state = generateState();
+
+      const url = github.createAuthorizationURL(state, ["user:email"]);
 
       setCookie("github_oauth_state", state, {
         path: "/",
-        secure: process.env.NODE_ENV !== "development",
+        secure: env.NODE_ENV === "production",
         httpOnly: true,
         maxAge: 60 * 10, // 10 minutes
         sameSite: "lax",
       });
 
+      setHeader("Location", url.toString());
+
       return new Response(null, {
         status: 302,
-        headers: {
-          Location: url.toString(),
-        },
       });
     } catch (error) {
       console.error("GitHub Upgrade Error:", error);
