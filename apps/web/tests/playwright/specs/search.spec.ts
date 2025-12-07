27a92835eb575d42acf82376963585bb9e49842a
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-12-07 19:02:01 +0000

    chore: update e2e tests

--- apps/web/tests/playwright/specs/search.spec.ts
+++ apps/web/tests/playwright/specs/search.spec.ts
@@ -2,9 +2,32 @@ import { test } from "@playwright/test";
 import { SearchPage } from "../pages/SearchPage";
 
 test.describe("Search experience", () => {
-	test("lists relevant results for a known query", async ({ page }) => {
+	test("navigates from header search and shows results sections", async ({
+		page,
+	}) => {
 		const searchPage = new SearchPage(page);
+
+		await searchPage.searchFromHeader();
+		await searchPage.expectOnSearchPage();
+		await searchPage.waitForAnyResult();
+		await searchPage.expectFiltersReady();
+		await searchPage.expectResultSections();
+	});
+
+	test("filters results by track and time", async ({ page }) => {
+		const searchPage = new SearchPage(page);
+
 		await searchPage.goto();
-		await searchPage.expectResults();
+		await searchPage.expectOnSearchPage();
+		await searchPage.waitForAnyResult();
+		await searchPage.expectFiltersReady();
+
+		const selectedTrack = await searchPage.selectFirstTrackOption();
+		await searchPage.expectSearchParam("track", selectedTrack);
+		await searchPage.waitForResultsOrEmpty();
+
+		const selectedTime = await searchPage.selectFirstTimeOption();
+		await searchPage.expectSearchParam("time", selectedTime);
+		await searchPage.waitForResultsOrEmpty();
 	});
 });
