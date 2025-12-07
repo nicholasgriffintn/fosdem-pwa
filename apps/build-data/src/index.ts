0b02900742f0c1e501fd141421853bad463d55b5
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-12-07 06:03:28 +0000

    chore: add tests

--- apps/build-data/src/index.ts
+++ apps/build-data/src/index.ts
@@ -2,21 +2,19 @@ import * as Sentry from "@sentry/cloudflare";
 
 import { buildData } from "./lib/fosdem";
 
-const run = async (env: any) => {
-	const year = "2026";
-	const data = await buildData({ year });
+const YEAR = "2026";
 
-	await env.R2.put(`fosdem-${year}.json`, JSON.stringify(data, null, 2));
+const run = async (env: any) => {
+	const data = await buildData({ year: YEAR });
+
+	await env.R2.put(`fosdem-${YEAR}.json`, JSON.stringify(data, null, 2));
 
 	return data;
 };
 
 export default Sentry.withSentry(
-	env => ({
+	(env: Env) => ({
 		dsn: "https://828f2b60a22b55556e8be6aa87517acf@o4508599344365568.ingest.de.sentry.io/4508734045814864",
-		// Set tracesSampleRate to 1.0 to capture 100% of spans for tracing.
-		// Learn more at
-		// https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
 		tracesSampleRate: 1.0,
 	}),
 	{
