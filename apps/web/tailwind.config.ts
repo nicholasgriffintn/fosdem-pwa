a9edb21d3aad0a2abce305daa5ae52135e2565cc
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-12-23 21:42:49 +0000

    chore: adjust container sizes

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -5,21 +5,26 @@ export default {
   darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1224px",
+      xl: "1400px",
+      "2xl": "1600px",
+    },
     container: {
       center: true,
       padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
       },
       screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-        '2xl': '1536px',
+        lg: "1224px",
+        xl: "1400px",
+        "2xl": "1600px",
       },
     },
     extend: {
