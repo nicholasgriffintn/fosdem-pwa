434908b479023f9fb6d5067a7d9d6c1eff1c2d3f
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-12-23 02:00:40 +0000

    chore: update description

--- apps/web/src/constants/site.ts
+++ apps/web/src/constants/site.ts
@@ -1,6 +1,6 @@
 export const siteMeta = {
 	title: "FOSDEM PWA",
-	description: "A companion app for the FOSDEM conference",
+	description: "A companion app for the FOSDEM conference for everyone to use",
 	locale: "en",
 	themeColor: "#0f172a",
 };
